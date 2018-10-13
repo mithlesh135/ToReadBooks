19fef745c8cf863ca064e245799c037aa6b51e20
Singh, Mithlesh <Mithlesh.Singh@cdk.com>
2018-10-13 17:37:36 +0530

    to read books

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
-import BookListContainer from "./container/BookList";
-import AddBookContainer from "./container/AddBook";
+import BookList from "./container/BookList";
+import AddBook from "./container/AddBook";
 import BookStore from "./reducers/BookStore";
 
 
 ReactDOM.render(
   <Provider store={createStore(BookStore)}>
     <div>
-      <AddBookContainer /> 
-      <BookListContainer />
+      <AddBook /> 
+      <BookList />
     </div>
   </Provider>, document.querySelector("div.container")
 );
